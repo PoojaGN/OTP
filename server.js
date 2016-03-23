fde298309c0af95cfc7d2c2466a52e35c2d8a147
PoojaGN <poojagn.pooja@gmail.com>
2016-03-23 23:32:42 +0530

    Added files via upload

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
-// $npm install express
-// $node server
-// var express=require("express");
-// var bp=require("body-parser");
-// var app=express();
-// app.use(bp.json());
-// app.use(exrpress.static(__dirname));
-// 
-// var data={};
-// app.get("/heros",function(req,res){
-	// res.json(data);
-// 	
-// app.get("*",function(req,res){
-	// res.sendFile(__dirname+"/index.html");
-// });
-// app.listen("9090");
-// console.log("your server is now running on port 9090");
+var express = require ("express");
+var app=express();
+	app.use(express.static(__dirname));
+app.get("/hero", function(request,response){
+	response.send("hello from express");
+});
 
-
-var express = require("express");
-var bp = require("body-parser");
-var app = express();
-    app.use(bp.json());
-    app.use(express.static(__dirname));
-    
-var data = {};
-    app.get("/heros", function(req,res){
-        res.json(data);
-    })
-    app.get("*", function(req,res){
-        res.sendFile(__dirname+"/index.html");
-    })
-    app.listen("9090");
-    console.log("the server is now running on prot 9090");
+app.get("/", function(request,response){
+	response.send();
+});
+app.listen(3000);
+console.log("your server is now running on 2233");
